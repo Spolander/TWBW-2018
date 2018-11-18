1b55d306494d7f811eaad98cff3d4ba6eb27ad3f
Joonas Vuorela <Joonas Vuorela@ictlab.local>
2018-11-18 05:33:37 +0200

    testuu

---
+++ Brainwash/Assets/Scripts/AnimationEvents.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AnimationEvents : MonoBehaviour {
+
+    public void FootStep()
+    {
+        soundEngine.instance.PlaySoundAt(transform.position, soundEngine.SoundClip.Footstep);
+    }
+}
---
+++ Brainwash/Assets/Scripts/soundEngine.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Audio;
+public class soundEngine : MonoBehaviour {
+
+    public static soundEngine instance;
+
+    public enum SoundClip {Footstep, GunSound, RightAnswer, WrongAnswer};
+
+
+    [SerializeField]
+    private AudioMixer mixer;
+
+
+    [SerializeField]
+    private AudioClip[] footSteps;
+
+    [SerializeField]
+    private AudioClip gunSound;
+
+    private void Awake()
+    {
+        instance = this;
+    }
+
+
+    public void PlaySoundAt(Vector3 pos, SoundClip clip)
+    {
+
+        GameObject g = new GameObject("sound clip");
+        AudioSource a = g.AddComponent<AudioSource>();
+
+        a.outputAudioMixerGroup = mixer.FindMatchingGroups("FX")[0];
+        a.spatialBlend = 1;
+
+        a.maxDistance = 15;
+
+        if (clip == SoundClip.Footstep)
+        {
+            a.clip = footSteps[Random.Range(0, footSteps.Length - 1)];
+        }
+        else if (clip == SoundClip.GunSound)
+        {
+            a.clip = gunSound;
+        }
+
+        g.transform.position = pos;
+        a.Play();
+
+        if (a.clip)
+            Destroy(g, a.clip.length);
+
+    }
+}
